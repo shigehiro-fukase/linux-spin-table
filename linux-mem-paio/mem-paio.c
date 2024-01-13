74f6e117526faeb04df79bb4968b7d54f30e5025
Shigehiro Fukase <shigehiro.fukase@gmail.com>
2024-01-14 03:10:13 +0900

    mem-paio: fix typo(s) of WX message

--- linux-mem-paio/mem-paio.c
+++ linux-mem-paio/mem-paio.c
@@ -111,51 +111,51 @@ static int parse_args(int argc, const char * argv[]) {
     if (opt_verbose) printf("VA=0x%p"NL, va);
 
     if (dir == 0) {
-	    unsigned long off;
-	    for (off=0; off<count; off++) {
-		    if (cmd == CMD_RB) {
-			    uint8_t * p = ((uint8_t*)va) + off;
-			    printf("RB 0x%08lX: 0x%02X"NL, phys_addr+ (off*byte_width), *p);
-		    } else if (cmd == CMD_RW) {
-			    uint16_t * p = ((uint16_t*)va) + off;
-			    printf("RW 0x%08lX: 0x%04X"NL, phys_addr+ (off*byte_width), *p);
-		    } else if (cmd == CMD_RD) {
-			    uint32_t * p = ((uint32_t*)va) + off;
-			    printf("RD 0x%08lX: 0x%08X"NL, phys_addr+ (off*byte_width), *p);
-		    } else if (cmd == CMD_RQ) {
-			    uint64_t * p = ((uint64_t*)va) + off;
-			    printf("RQ 0x%08lX: 0x%016lX"NL, phys_addr+ (off*byte_width), *p);
-		    }
-	    }
+        unsigned long off;
+        for (off=0; off<count; off++) {
+            if (cmd == CMD_RB) {
+                uint8_t * p = ((uint8_t*)va) + off;
+                printf("RB 0x%08lX: 0x%02X"NL, phys_addr+ (off*byte_width), *p);
+            } else if (cmd == CMD_RW) {
+                uint16_t * p = ((uint16_t*)va) + off;
+                printf("RW 0x%08lX: 0x%04X"NL, phys_addr+ (off*byte_width), *p);
+            } else if (cmd == CMD_RD) {
+                uint32_t * p = ((uint32_t*)va) + off;
+                printf("RD 0x%08lX: 0x%08X"NL, phys_addr+ (off*byte_width), *p);
+            } else if (cmd == CMD_RQ) {
+                uint64_t * p = ((uint64_t*)va) + off;
+                printf("RQ 0x%08lX: 0x%016lX"NL, phys_addr+ (off*byte_width), *p);
+            }
+        }
     } else /* if (dir == 1) */ {
-	    count = argc - i;
-	    printf("count=%ld"NL, count);
-	    unsigned long off;
-	    for (off=0; i<argc; i++, off++) {
+        count = argc - i;
+        printf("count=%ld"NL, count);
+        unsigned long off;
+        for (off=0; i<argc; i++, off++) {
             value = strtoull(argv[i], NULL, 0);
-		    if (opt_verbose) printf("argv[%d]=0x%llX ret=%d"NL, i, value, ret);
-		    if (cmd == CMD_WB) {
-			    uint8_t * p = ((uint8_t*)va) + off;
-			    uint8_t prev = *p;
-			    *p = (uint8_t)(value & 0xFFu);
-			    printf("WB 0x%08lX: 0x%02X <- 0x%02X"NL, phys_addr+ (off*byte_width), prev, *p);
-		    } else if (cmd == CMD_WW) {
-			    uint16_t * p = ((uint16_t*)va) + off;
-			    uint16_t prev = *p;
-			    *p = (uint16_t)(value & 0xFFFFu);
-			    printf("WB 0x%08lX: 0x%04X <- 0x%04X"NL, phys_addr+ (off*byte_width), prev, *p);
-		    } else if (cmd == CMD_WD) {
-			    uint32_t * p = ((uint32_t*)va) + off;
-			    uint32_t prev = *p;
-			    *p = (uint32_t)(value & 0xFFFFFFFFul);
-			    printf("WB 0x%08lX: 0x%08X <- 0x%08X"NL, phys_addr+ (off*byte_width), prev, *p);
-		    } else if (cmd == CMD_WQ) {
-			    uint64_t * p = ((uint64_t*)va) + off;
-			    uint64_t prev = *p;
-			    *p = (uint64_t)(value & 0xFFFFFFFFFFFFFFFFull);
-			    printf("WB 0x%08lX: 0x%016lX <- 0x%016lX"NL, phys_addr+ (off*byte_width), prev, *p);
-		    }
-	    }
+            if (opt_verbose) printf("argv[%d]=0x%llX ret=%d"NL, i, value, ret);
+            if (cmd == CMD_WB) {
+                uint8_t * p = ((uint8_t*)va) + off;
+                uint8_t prev = *p;
+                *p = (uint8_t)(value & 0xFFu);
+                printf("WB 0x%08lX: 0x%02X <- 0x%02X"NL, phys_addr+ (off*byte_width), prev, *p);
+            } else if (cmd == CMD_WW) {
+                uint16_t * p = ((uint16_t*)va) + off;
+                uint16_t prev = *p;
+                *p = (uint16_t)(value & 0xFFFFu);
+                printf("WW 0x%08lX: 0x%04X <- 0x%04X"NL, phys_addr+ (off*byte_width), prev, *p);
+            } else if (cmd == CMD_WD) {
+                uint32_t * p = ((uint32_t*)va) + off;
+                uint32_t prev = *p;
+                *p = (uint32_t)(value & 0xFFFFFFFFul);
+                printf("WD 0x%08lX: 0x%08X <- 0x%08X"NL, phys_addr+ (off*byte_width), prev, *p);
+            } else if (cmd == CMD_WQ) {
+                uint64_t * p = ((uint64_t*)va) + off;
+                uint64_t prev = *p;
+                *p = (uint64_t)(value & 0xFFFFFFFFFFFFFFFFull);
+                printf("WQ 0x%08lX: 0x%016lX <- 0x%016lX"NL, phys_addr+ (off*byte_width), prev, *p);
+            }
+        }
     }
     munmap_device_memory(mmap_fd);
     return ret;
